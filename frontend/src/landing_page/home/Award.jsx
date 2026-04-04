d73ff694bf7c4e279093ca4ac8950ff535e491f9
Sparshkr0908 <sparshkumar0410@gmail.com>
2026-04-05 01:24:13 +0530

    Hero and Award has modified properly little bit of styling is still necessary that will be done once the home section is complete

--- frontend/src/landing_page/home/Award.jsx
+++ frontend/src/landing_page/home/Award.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 function Award() {
     return (  
-        <h1>
-           Award <i class="fa-solid fa-circle-user"></i>
-        </h1>
+           <div className='container mt-5'>
+                <div className='row'>
+                    <div className='col-6 p-5'>
+                        <img src="public/media/image/largestBroker.svg"/>
+                    </div>
+                    <div className='col-6 p-5 mt-5'>
+                        <h2>Largest Stock Broker in India</h2>
+                        <p className='mb-5'>2+ million Zerodha clients contribute to over 15% of all retail order volumes in india daily by trading and investing in: </p>
+                        <div className='row'>
+                            <div className='col-6'>
+                                <ul>
+                                    <li>
+                                        <p>Futures and options</p>
+                                    </li>
+                                    <li>
+                                        <p>Commodity derivatives</p>
+                                    </li>
+                                    <li>
+                                        <p>Curreny derivatives</p>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className='col-6'>
+                                <ul>
+                                    <li>
+                                        <p>Stocks and IPOs</p>
+                                    </li>
+                                    <li>
+                                        <p>Direct mutuals funds</p>
+                                    </li>
+                                    <li>
+                                        <p>Bonds and Govts. Securities</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <img style={{width:"90%"}} src="media\Image\pressLogos.png"/>
+                    </div>
+                </div>
+           </div>
     );
 }
 
