e437155923c603952fcb6ce8b9a05b6b21cac6fb
Sparshkr0908 <sparshkumar0410@gmail.com>
2026-05-06 00:51:37 +0530

    Built Kite dashboard UI with initial components and basic layout

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import "./index.css";
+import Home from "./components/Home";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/*" element = {<Home/>}/>  //we are making it nested so that any changes we did, done within the component, we don't need to change or reload the application
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
