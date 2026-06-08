2f89183669b65b143cabaf6f24ba0cedca840573
Sparshkr0908 <sparshkumar0410@gmail.com>
2026-06-08 16:31:29 +0530

    update positions, holding along with adding dummy data.

---
+++ dashboard/src/data/data.jsx
@@ -0,0 +1,91 @@
+export const watchlist = [
+  {
+    name: "INFY",
+    price:1555.45,
+    percent:"-1.68%",
+    isDown: True,
+  },
+  {
+    name: "MEESHO",
+    price:192.80,
+    percent:"-5.34",
+    isDown: True,
+  },
+  {
+    name: "TATASILV",
+    price:23.65,
+    percent:"+1.89%",
+    isDown: False,
+  },
+  {
+    name: "Vedanta",
+    price:310.90,
+    percent:"+2.30%",
+    isDown: False,
+  },
+  {
+    name:"IDEA",
+    price:11.24,
+    percent:"+0.43",
+    isDown:False,
+  },
+]
+export const holdings = [
+  {
+    name: "IDEA",
+    gty: 2,
+    avg: 11.00,
+    price:11.24,
+    net:"+2.18",
+    day: "+0.43",
+  },
+  {
+    name: "INFY",
+    gty: 2,
+    avg: 1551.23,
+    price:1555.45,
+    net:"+8.2",
+    day: "-2.3",
+  },
+  {
+    name: "MEESO",
+    gty: 2,
+    avg: 194.80,
+    price:192.80,
+    net:"-1.03",
+    day: "-5.34",
+  },
+]
+
+export const position = [
+  {
+    name: "INFY",
+    price:1555.45,
+    percent:"-1.68%",
+    isDown: True,
+  },
+  {
+    name: "MEESHO",
+    price:192.80,
+    percent:"-5.34",
+    isDown: True,
+  },
+  {
+    name: "TATASILV",
+    price:23.65,
+    percent:"+1.89%",
+    isDown: False,
+  },
+  {
+    name: "Vedanta",
+    price:310.90,
+    percent:"+2.30%",
+    isDown: False,
+  },
+  {
+    name:"IDEA",
+    price:11.24,
+    percent:"+0.43",
+    isDown:False,
+  },
+]
