c5bff810047964078eeed0da5ebdfc7e2bf7ef09
Sparshkr0908 <sparshkumar0410@gmail.com>
2026-07-02 17:24:08 +0530

    Adding Backend for Signup

---
+++ backend/schemas/frontendSignupSchema.js
@@ -0,0 +1,28 @@
+import { Schema } from "mongoose";
+import bcryptjs from "bcrypt";
+
+export const userSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, "Your email address is required"],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, "Your username is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Your password is required"],
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 12);
+});
+
+export {userSchema};
