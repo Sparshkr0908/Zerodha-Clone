2cf8bff3e0c2f09ccf2e20fb67a959e969b4f463
Sparshkr0908 <sparshkumar0410@gmail.com>
2026-07-19 18:35:15 +0530

    make the order component dynamic

--- dashboard/src/components/orders.jsx
+++ dashboard/src/components/orders.jsx
@@ -1,15 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Orders = () => {
-  return (
+  const [allOrders, setAllOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5501/allOrders").then((res) => {
+      setAllOrders(res.data);
+    });
+  }, []);
+
+  return allOrders.length === 0 ? (
     <div className="orders">
       <div className="no-orders">
         <p>You haven't placed any orders today</p>
-
-        <Link to={"/"} className="btn">
-          Get started
-        </Link>
+        <Link to={"/"} className="btn">Get started</Link>
+      </div>
+    </div>
+  ) : (
+    <div className="orders">
+      <h3 className="title">Orders ({allOrders.length})</h3>
+      <div className="order-table">
+        <table>
+          <tr>
+            <th>Instrument</th>
+            <th>Qty.</th>
+            <th>Price</th>
+            <th>Mode</th>
+          </tr>
+          {allOrders.map((order, index) => (
+            <tr key={index}>
+              <td>{order.name}</td>
+              <td>{order.qty}</td>
+              <td>{order.prices}</td>
+              <td>{order.mode}</td>
+            </tr>
+          ))}
+        </table>
       </div>
     </div>
   );
--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import "./index.css";
 import Home from "./components/Home";
+import Login from "./components/page/Login";      
+import Signup from "./components/page/Signup";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
+      <Route path="/login" element={<Login />} />     
+        <Route path="/signup" element={<Signup />} />  
       <Route path = "/*" element = {<Home/>}/>  //we are making it nested so that any changes we did, done within the component, we don't need to change or reload the application
     </Routes>
     </BrowserRouter>
